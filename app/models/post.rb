4191427d9473b08439069d3a18fc608d9c57a6a1
Jacob Garcia <janikgar@gmail.com>
2018-07-11 15:43:16 -0500

    Added image upload, navbar updates

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
+	mount_uploader :picture, PictureUploader
+
 	has_many :comments
 	belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_11_155217) do
+ActiveRecord::Schema.define(version: 2018_07_11_195630) do
 
   create_table "comments", force: :cascade do |t|
     t.string "author"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2018_07_11_155217) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.string "picture"
   end
 
   create_table "users", force: :cascade do |t|
